6264d31441b4f2d06323f2b84ac22fbf2e7dcd95
jpparreirap <jpparreirap@gmail.com>
2022-06-26 22:34:02 -0300

    Beer API

---
+++ SpringBoot/projetinhos/beer_api/src/main/java/one/digitalinnovation/beerstock/BeerstockApplication.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.beerstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan
+public class BeerstockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerstockApplication.class, args);
+	}
+
+}
